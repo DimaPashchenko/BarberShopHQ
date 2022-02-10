a17aad116479873dd2fc62a0f1078badd5bc5a06
DimaPashchenko <kitaminchik@gmail.com>
2022-02-10 18:25:32 +0200

    Update Gemfile, app.rb

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
-class Client < ActiveRecord::database
+class Client < ActiveRecord::Base
 end
 
 get '/' do
